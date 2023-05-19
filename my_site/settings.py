6dcce0c04d24dfab670a709438353a294b485d86
waccabi <70471316+waccabi@users.noreply.github.com>
2023-05-19 13:25:21 +0700

    Update settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3wd@wh8dqf8d41ibwe#@onru3p@4^b%n-9x4_4!dq&&v4rixnw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['webpp-production.up.railway.app']
+ALLOWED_HOSTS = ['webpp-production.up.railway.app','www.webpp-production.up.railway.app']
 
 
 # Application definition
